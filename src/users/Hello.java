2bf4255d220aa36716192a4e934ee6455ae045fe
Nursultan Ulan uulu <ulanuulunursultan36@gmail.com>
2025-04-24 18:01:11 +0000

    Hello деген соз жаздым

---
+++ src/users/Hello.java
@@ -0,0 +1,12 @@
+package users;
+
+public class Hello {
+//    psvm
+    public static void main(String[] args) {
+        System.out.println("Hello nurs");
+        System.out.println("Hello nurs");
+        System.out.print("Hello nurs");
+        System.out.print("Hello nurs");
+
+    }
+}
